c6fda83bab77eeaee2bdc1e94cee57c3605f4c66
Sarah Pak <52671374+sarahspak@users.noreply.github.com>
2023-05-01 21:20:16 -0400

    rename and add new script

---
+++ get_lmflow_requirements.sh
@@ -0,0 +1,5 @@
+cd /home/user/ubuntu/LMFlow
+pip install -r requirements.txt
+
+# add python packages installed to PATH
+echo "EXPORT PATH=/home/ubuntu/.local/bin:$PATH" >> ~/.bashrc && source ~/.bashrc
