d48d3a3aec76e8e3708b7831e7237e128652d84b
Sarah Pak <52671374+sarahspak@users.noreply.github.com>
2023-05-01 21:33:37 -0400

    added a pip install -e for lmflow

--- get_lmflow_requirements.sh
+++ get_lmflow_requirements.sh
@@ -3,3 +3,6 @@ pip install -r requirements.txt
 
 # add python packages installed to PATH
 echo "export PATH=/home/ubuntu/.local/bin:$PATH" >> ~/.bashrc && source ~/.bashrc
+
+# finally, install LMFlow as a pip package locally
+cd /home/ubuntu/LMFlow && pip install -e .
