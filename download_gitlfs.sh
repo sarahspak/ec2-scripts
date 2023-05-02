8807c2b7aa27287c15fc7c869efd51cec29b2239
Pak <sarahpak@amazon.com>
2023-05-01 20:59:33 -0400

    first commit

---
+++ download_gitlfs.sh
@@ -0,0 +1,3 @@
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | sudo bash
+sudo apt install git-lfs
+git lfs install
---
+++ download_models.sh
@@ -0,0 +1,5 @@
+# download models
+git clone https://huggingface.co/decapoda-research/llama-7b-hf
+git clone https://huggingface.co/decapoda-research/llama-13b-hf
+# git clone https://huggingface.co/lmsys/vicuna-7b-delta-v0
+git clone https://huggingface.co/datasets/anon8231489123/ShareGPT_Vicuna_unfiltered
